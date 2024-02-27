6d6fa95d13ad2b2baec41ae7fc24c151d6312ca2
Micah <micahhlc@gmail.com>
2024-02-28 00:29:23 +0900

    add summary

--- fibonacci_number.py
+++ fibonacci_number.py
@@ -20,6 +20,7 @@ def fibonacci_recursion(n_rounds):
     curr_n = 1
     count = 2
     final_list = []
+
     def inner_function(n_rounds, prev_n, curr_n, count, final_list):
         if count <= n_rounds:
             newFibo = prev_n + curr_n
@@ -28,18 +29,30 @@ def fibonacci_recursion(n_rounds):
             prev_n = curr_n
             curr_n = newFibo
             count += 1
-            inner_function(n_rounds, prev_n, curr_n, count, final_list)
+            return inner_function(n_rounds, prev_n, curr_n, count, final_list)
         else:
             return final_list
+        
     return inner_function(n_rounds, prev_n, curr_n, count, final_list)
 
-result = fibonacci_recursion(5)
-print(result)  # Output will be 15
+result = fibonacci_recursion(15)
+print(result)  
 
 
 
-
-
-# fibonacci_loop()
-
-#Python
+# Nth	Fibonacci number	Total
+# 1	0	
+# 2	1	1
+# 3	1	2
+# 4	2	3
+# 5	3	5
+# 6	5	8
+# 7	8	13
+# 8	13	21
+# 9	21	34
+# 10	34	55
+# 11	55	89
+# 12	89	144
+# 13	144	233
+# 14	233	377
+# 15	377	610
