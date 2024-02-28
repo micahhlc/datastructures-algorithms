9efad1be00a063c8bc8756996667fa494ad2acc1
Micah <micahhlc@gmail.com>
2024-02-29 01:04:57 +0900

    update fibonacci different approaches

--- fibonacci_number.py
+++ fibonacci_number.py
@@ -1,44 +1,74 @@
+# W3CSchool samples
+# https://www.w3schools.com/dsa/dsa_algo_simple.php 
 
-def fibonacci_loop():
+import time
+
+# fibonacci through loop
+def fibonacci_loop(n):
     prev2 = 0
     prev1 = 1
-
-    print(prev2)
-    print(prev1)
-    for fibo in range(18):
+    newFibo = 0
+    # print(prev2)
+    # print(prev1)
+    for fibo in range(n):
         newFibo = prev1 + prev2
-        print(newFibo)
+        # print(newFibo)
         prev2 = prev1
         prev1 = newFibo
+    return(newFibo)
 
-
-# fibonacci_recursion
-def fibonacci_recursion(n_rounds):
-    print(0)
-    print(1)
+# fibonacci_recursion from loop
+def fibonacci_recursion(n):
+    # print(0)
+    # print(1)
     prev_n = 0
     curr_n = 1
     count = 2
-    final_list = []
+    final_list = [0,1]
 
-    def inner_function(n_rounds, prev_n, curr_n, count, final_list):
-        if count <= n_rounds:
+    def inner_function(n, prev_n, curr_n, count, final_list):
+        if count <= n:
             newFibo = prev_n + curr_n
-            print(newFibo)
+            # print(newFibo)
             final_list.append(newFibo)
             prev_n = curr_n
             curr_n = newFibo
             count += 1
-            return inner_function(n_rounds, prev_n, curr_n, count, final_list)
+            return inner_function(n, prev_n, curr_n, count, final_list)
         else:
             return final_list
         
-    return inner_function(n_rounds, prev_n, curr_n, count, final_list)
+    return inner_function(n, prev_n, curr_n, count, final_list)
 
-result = fibonacci_recursion(15)
-print(result)  
+# full recursion
+def fibonacci_fullrecursion(n):    
+    if n <= 1: 
+        return n
+    else:
+        return (fibonacci_fullrecursion(n - 1) + fibonacci_fullrecursion(n - 2))
 
+# recursion without counter
+def fibo(n, flist = [0, 1]):
+    flist_qty = len(flist)
+    if n <= 1: 
+        return n
+    elif n == flist_qty:
+        new_fibon_no = flist[n-1] + flist[n-2]
+        return new_fibon_no
+    else:
+        flist.append(flist[flist_qty - 1] + flist[flist_qty - 2])
+        print(flist)
+        return fibo(n)
 
+def fibo_runtime(func_name):
+    start_time = time.time()
+    for i in range(20):
+        print(f"F{i+1}: {func_name(i)}")
+    end_time = time.time()
+    elapsed_time = end_time - start_time
+    print(func_name, ": Elapsed time:", elapsed_time * 1000, "milliseconds")
+
+fibo_runtime(fibonacci_loop)
 
 # Nth	Fibonacci number	Total
 # 1	0	
--- test.py
+++ test.py
@@ -1,25 +1,16 @@
-# def fibo(n):    
-#     if n == 3 or n == 2:
-#         return 1;
-#     elif n <= 1: 
-#         return 0;
-#     else:
-#         return (fibo(n - 1) + fibo(n - 2))
-    
-# for i in range(10):
-#     print(f"F{i+1}: {fibo(i+1)}")
-
-
 def fibo(n, flist = [0, 1]):
     flist_qty = len(flist)
-    if n == 1: 
-        return 0
-    elif n == 2: 
-        return 1
-    elif n == flist_qty + 1:
-        new_fibon_no = flist[n-1-1] + flist[n-2-1]
+    if n <= 1: 
+        return n
+    elif n == flist_qty:
+        new_fibon_no = flist[n-1] + flist[n-2]
         return new_fibon_no
     else:
-        flist.append(flist[n-n-1-1] + flist[n-n-1-1])
+        flist.append(flist[flist_qty - 1] + flist[flist_qty - 2])
+        print(flist)
         return fibo(n)
-print(fibo(5))
+
+for i in range(10):
+    print(f"F{i+1}: {fibo(i)}")
+
+
